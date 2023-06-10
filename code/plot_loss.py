69c5a5dfbe4bffdfd3758d8643a4daab02361ee1
Espen Lønes <espen.lones@fys.uio.no>
2023-06-10 17:31:36 +0200

    Better loss plot figures

--- code/plot_loss.py
+++ code/plot_loss.py
@@ -1,17 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scienceplots
 
 def plot(model):
     data_path = 'C:/Users/espen/Documents/Skole/MASTER/code/data/models/' + model + '.npz'
     data = np.load(data_path) 
     val_losses = data['vallosses']; train_losses = data['trainlosses']
 
-    plt.plot(train_losses, label='Traning', color='red', linestyle='dashed')
-    plt.plot(val_losses, label='Validation', color='blue', linestyle='solid')
-    plt.xlabel('Epoch')
-    plt.ylabel('MSE loss')
-    plt.legend()
-    plt.show()
+    with plt.style.context(['science','grid']):
+        fig, ax = plt.subplots()
+        ax.plot(train_losses, label='Traning', color='red', linestyle='dashed')
+        ax.plot(val_losses, label='Validation', color='blue', linestyle='solid')
+        ax.autoscale(tight=True)
+        plt.xlabel('Epoch')
+        plt.ylabel('MSE loss')
+        plt.legend()
+        #plt.show()
+        fig.savefig('images/lossplot_' + model + '.svg', dpi=300)
+        plt.close()
 
 def print_config(model):
     data_path = 'C:/Users/espen/Documents/Skole/MASTER/code/data/models/' + model + '.npz'
@@ -21,15 +27,15 @@ def print_config(model):
 
 
 if __name__=='__main__':
-    #plot("Test2")
-    #plot('Test3(upper_triangular)')
-    #plot('Test4')
-    #plot('Test5(Two_images)')
-    #plot('Test6(hypoechoic)')
-    #plot('Test7(hyperechoic)')
-    #plot('Test8(Verasonics)')
-    #plot('Test9(Verasonics-complex_network)')
-    #plot('Test10(Verasonics-complex_network2)')
-    #plot('Test11(Verasonics-complex_network_4_frames)')
+    plot("Test2")
+    plot('Test3(upper_triangular)')
+    plot('Test4')
+    plot('Test5(Two_images)')
+    plot('Test6(hypoechoic)')
+    plot('Test7(hyperechoic)')
+    plot('Test8(Verasonics)')
+    plot('Test9(Verasonics-complex_network)')
+    plot('Test10(Verasonics-complex_network2)')
+    plot('Test11(Verasonics-complex_network_4_frames)')
     plot('Test12(Verasonics-small_network_4_frames)')
     #print_config("Test3(upper_triangular)")
